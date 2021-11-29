1421e4030e9c24663dcc957e4abb40bdbe7b7366
Vitor Hugo da Silva Lima <vitor.lima@cornershopapp.com>
2021-11-29 13:58:40 -0500

    feat(analysis): Adding CommitData model

--- backend/apps/analysis/models.py
+++ backend/apps/analysis/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class CommitData(models.Model):
+    repository = models.CharField(max_length=20)
+    author = models.CharField(max_length=100, default="")
+    hash = models.CharField(max_length=200)
+    date = models.DateTimeField()
+    message = models.TextField()
